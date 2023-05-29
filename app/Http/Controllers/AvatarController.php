888d61ac0151467f64ef8f285375338b3df4dc2e
Andrés Arango <97069530+andresxd07@users.noreply.github.com>
2023-05-29 11:25:30 -0500

    Update AvatarController.php

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -7,20 +7,20 @@ class AvatarController extends Controller
     /** Return view Avatar */
     public function avatar()
     {
-        return view('Books.Avatar_pick');
+        return view('books.Avatar_pick');
     }
 
     /**Return avatar male edit*/
 
     public function avatarMale()
     {
-        return view('Books.Avatar_male');
+        return view('books.Avatar_male');
     }
 
     /**Return avatar female edit*/
 
     public function avatarFemale()
     {
-        return view('Books.Avatar_female');
+        return view('books.Avatar_female');
     }
 }
